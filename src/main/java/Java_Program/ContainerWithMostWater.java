0b6a0539ce3affee3bc8e572336f452697595c9c
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2024-12-26 23:53:39 +0530

    Update

---
+++ src/main/java/Java_Program/ContainerWithMostWater.java
@@ -0,0 +1,40 @@
+package Java_Program;
+
+public class ContainerWithMostWater {
+
+    public static int toGetMaximumWater(int[] num){
+
+        int left=0;
+        int right= num.length-1;
+        int max=0,temp=0;
+
+            while (left < right) {
+                if (num[left] < num[right]) {
+                    temp = num[left] * (right - left);
+                    left++;
+
+                } else {
+                    temp = num[right] * (right - left);
+                    right--;
+                }
+
+                if (temp > max)
+                    max = temp;
+
+            }
+
+
+        return max;
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] num={1,8,6,2,5,4,8,3,7,9};
+
+        System.out.println("Maximum Unit of Water :"+ContainerWithMostWater.toGetMaximumWater(num));
+
+
+
+    }
+}

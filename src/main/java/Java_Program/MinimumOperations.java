d0356e4547d09105a7217829ac508b08edb4fb37
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2024-12-30 11:10:02 +0530

    Dec 30th

---
+++ src/main/java/Java_Program/MinimumOperations.java
@@ -0,0 +1,40 @@
+package Java_Program;
+
+import java.util.Arrays;
+
+
+public class MinimumOperations {
+
+    public static int minimumOperations(int[] nums) {
+        int count=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]%3==0){
+
+            }
+            else {
+                int temp=nums[i]%3;
+                nums[i]=nums[i]-temp;
+                count++;
+            }
+        }
+        System.out.println("Output :"+ Arrays.toString(nums));
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={3,6,9};
+        System.out.println(minimumOperations(nums));
+
+        String[] name={"hi","hello"};
+        if(name[1].contains("e")){
+            System.out.println("Hello");
+        }else{
+            System.out.println("Hi");
+        }
+    }
+
+    }
+
+
+
+

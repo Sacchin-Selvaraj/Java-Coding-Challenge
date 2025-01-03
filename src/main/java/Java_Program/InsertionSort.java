fec0450b7818a6608567711ce6ea843505396c8a
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-03 10:43:09 +0530

    Insertion Sort

---
+++ src/main/java/Java_Program/InsertionSort.java
@@ -0,0 +1,30 @@
+package Java_Program;
+
+public class InsertionSort {
+// Insertion Sort
+    public static void toSort(int[] colour){
+
+        for(int i=1;i< colour.length;i++){
+
+            int key=colour[i];
+            int j=i-1;
+            while(j>=0&&colour[j]>key){
+                colour[j+1]=colour[j];
+                j--;
+            }
+            colour[j+1]=key;
+        }
+
+
+        for(int num:colour){
+            System.out.print(num+" ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] colour={5,4,3,2,6,9,4,2,1,0};
+        InsertionSort.toSort(colour);
+
+    }
+}

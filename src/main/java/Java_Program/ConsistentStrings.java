9c24fe149d24b4e28bbbe793789c60b1607e7d4d
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-06 23:35:17 +0530

    Strings !!!

---
+++ src/main/java/Java_Program/ConsistentStrings.java
@@ -0,0 +1,21 @@
+package Java_Program;
+
+public class ConsistentStrings {
+
+    public static int toCount(String allowed,String[] words){
+        int count=0;
+        for (String word:words) {
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String allowed="abc";
+        String[] words={"a","b","c","ab","bcd"};
+
+        System.out.println(toCount(allowed,words));
+
+    }
+}

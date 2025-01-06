90720371cdc6359d2a5a50a0db17ae5a0089c63d
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-06 23:44:59 +0530

    Strings!!!

--- src/main/java/Java_Program/ConsistentStrings.java
+++ src/main/java/Java_Program/ConsistentStrings.java
@@ -1,13 +1,29 @@
 package Java_Program;
 
+import java.util.HashSet;
+
 public class ConsistentStrings {
 
     public static int toCount(String allowed,String[] words){
         int count=0;
-        for (String word:words) {
+        HashSet<Character> set=new HashSet<>();
 
-
+        for (char temp:allowed.toCharArray()){
+            set.add(temp);
         }
+        for (String word:words) {
+            int flag=1;
+            for (int i = 0; i < word.length(); i++) {
+                if(!set.contains(word.charAt(i))){
+                    flag=0;
+                    break;
+                }
+
+            }
+            count+=flag;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {

e85a7c12f858aa532a354df4e8e1a4946ec33b55
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-08 10:42:03 +0530

    Top K Elements

--- src/main/java/Java_Program/TopKElements.java
+++ src/main/java/Java_Program/TopKElements.java
@@ -1,23 +1,27 @@
 package Java_Program;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class TopKElements {
 
     public static void toFindKElement(int[] nums){
         PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        int[] result=new int[3];
 
         for (int i = 0; i < 3; i++) {
             minHeap.add(nums[i]);
         }
-
+        int j=0;
         for (int i = 3; i < 6; i++) {
             minHeap.add(nums[i]);
             if(minHeap.size()>3){
-                minHeap.poll();
+                result[j++]=minHeap.poll();
             }
         }
         System.out.println(minHeap);
+
+        System.out.println("Smallest Number :"+ Arrays.toString(result));
     }
 
     public static void main(String[] args) {

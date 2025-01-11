9feea3ca38b78426c5c1e9a47a89c65b6bae71d5
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-11 23:51:31 +0530

    Arrays

---
+++ src/main/java/Java_Program/MinimumGameNumber.java
@@ -0,0 +1,34 @@
+package Java_Program;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class MinimumGameNumber {
+    public static int[] numberGame(int[] nums) {
+
+        PriorityQueue<Integer> heap=new PriorityQueue<>();
+        int[] result=new int[nums.length];
+
+        for (int i:nums){
+            heap.add(i);
+        }
+        System.out.println(heap);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if(i%2==0){
+                result[i+1]=heap.poll();
+            }else{
+                result[i-1]=heap.poll();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={5,4,2,3};
+
+        System.out.println(Arrays.toString(numberGame(nums)));
+
+    }
+}
---
+++ src/main/java/Java_Program/ScoreOfAString.java
@@ -0,0 +1,28 @@
+package Java_Program;
+
+public class ScoreOfAString {
+
+    public static int scoreOfString(String s) {
+
+        int sum=0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if(s.charAt(i)-s.charAt(i+1)<0) {
+                int temp=s.charAt(i)-s.charAt(i+1);
+                sum = sum +temp*-1;
+                System.out.println(s.charAt(i) - s.charAt(i + 1));
+            }else{
+                sum = sum + (s.charAt(i) - s.charAt(i + 1));
+                System.out.println(s.charAt(i) - s.charAt(i + 1));
+            }
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        String s="zaz";
+
+        System.out.println(scoreOfString(s));
+
+    }
+}

0bddfecbea5a9672438f554836c0699bfde67061
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-11 23:55:16 +0530

    Integer!!!

---
+++ src/main/java/Java_Program/ReverseInteger.java
@@ -0,0 +1,27 @@
+package Java_Program;
+
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        int sum=0;
+        while(x!=0){
+            int temp=x%10;
+            x=x/10;
+            sum=(sum*10+temp);
+
+        }
+
+        System.out.println("Output :"+sum);
+        if(sum>2147483648l)
+            return 0;
+        else
+            return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int x=1534236469;
+        ReverseInteger obj=new ReverseInteger();
+        obj.reverse(x);
+    }
+}

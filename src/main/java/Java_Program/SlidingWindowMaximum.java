7a6ae38d4f9ad35d47dfb9042b0eeb98487e4019
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-18 10:42:41 +0530

    Deque

--- src/main/java/Java_Program/SlidingWindowMaximum.java
+++ src/main/java/Java_Program/SlidingWindowMaximum.java
@@ -6,18 +6,6 @@ import java.util.Scanner;
 
 // LeetCode 239
 public class SlidingWindowMaximum {
-    public static int[] findMaximum(int[] num,int k){
-        int Max=num[0];
-        for(int i=0;i<k;i++){
-            if(num[i]>Max)
-                Max=num[i];
-        }
-        int[] Sum=new int[num.length];
-        Sum[0]=Max;
-
-
-
-        return Sum;
 
 
     }

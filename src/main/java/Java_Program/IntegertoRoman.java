bc55c24d9cfceee9a673c0aefa75b3ae7103ce6b
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-20 23:47:46 +0530

    Strings

---
+++ src/main/java/Java_Program/IntegertoRoman.java
@@ -0,0 +1,14 @@
+package Java_Program;
+
+public class IntegertoRoman {
+
+    public static String intToRoman(int num) {
+
+
+    }
+
+    public static void main(String[] args) {
+        int num = 58;
+        System.out.println(intToRoman(num));
+    }
+}

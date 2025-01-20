d93558ffbc9b499b989401d3dc28a3baa4312829
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-21 00:56:03 +0530

    Anagram

--- src/main/java/Java_Program/GroupOfAnagrams.java
+++ src/main/java/Java_Program/GroupOfAnagrams.java
@@ -1,14 +1,27 @@
 package Java_Program;
 
-import java.util.List;
+import java.util.*;
 
 public class GroupOfAnagrams {
 
-    public List<List<String>> groupAnagrams(String[] strs) {
-
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String ,Integer>map=new HashMap<>();
+        List<List<String>> a = new ArrayList<>();
+        for (String str:strs){
+            char[] ch=str.toCharArray();
+            Arrays.sort(ch);
+            String temp=new String(ch);
+            if(map.containsKey(temp)){
+                a.get(map.get(temp)).add(str);
+            }else{
+                map.put(temp,a.size());
+                a.add(new ArrayList<>(List.of(str)));
+            }
+        }
+        return a;
     }
-
     public static void main(String[] args) {
-
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
     }
 }

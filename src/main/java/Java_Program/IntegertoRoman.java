717e3deb8ecba2f0b564175d8e35e570a88ee359
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-20 23:51:02 +0530

    Strings

--- src/main/java/Java_Program/IntegertoRoman.java
+++ src/main/java/Java_Program/IntegertoRoman.java
@@ -1,10 +1,20 @@
 package Java_Program;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IntegertoRoman {
 
     public static String intToRoman(int num) {
 
-        System.out.println(1994/10);
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
 
         return null;
     }

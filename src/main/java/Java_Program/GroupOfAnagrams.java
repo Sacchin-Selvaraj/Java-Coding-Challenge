ea5390cfa7e07e1a0983953128a81c2ba95f6220
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-21 00:21:01 +0530

    Strings

---
+++ src/main/java/Java_Program/GroupOfAnagrams.java
@@ -0,0 +1,14 @@
+package Java_Program;
+
+import java.util.List;
+
+public class GroupOfAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/Java_Program/IntegertoRoman.java
+++ src/main/java/Java_Program/IntegertoRoman.java
@@ -7,20 +7,47 @@ public class IntegertoRoman {
 
     public static String intToRoman(int num) {
 
-        Map<Character, Integer> m = new HashMap<>();
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-
-        return null;
+        StringBuilder res=new StringBuilder();
+        while(num>0) {
+            if (num > 3999)
+                return " ";
+            if (num > 1000) {
+                res.append("M");
+                num = num - 1000;
+            } else if (num>=900&&num<1000) {
+                res.append("CM");
+                num=num-900;
+            } else if (num>500 && num<900) {
+                res.append("D");
+                num-=500;
+            } else if (num>100&&num<500) {
+                res.append("C");
+                num-=100;
+            } else if (num>=90&&num<100) {
+                res.append("XC");
+                num=num-90;
+            }else if (num>50&&num<90) {
+                res.append("L");
+                num-=50;
+            }else if (num>10&&num<50) {
+                res.append("X");
+                num-=10;
+            }else if (num>5&&num<10) {
+                res.append("V");
+                num-=5;
+            }else if (num==4) {
+                res.append("IV");
+                num=num-4;
+            }else if (num>=1&&num<5) {
+                res.append("I");
+                num-=1;
+            }
+        }
+        return res.toString();
     }
 
     public static void main(String[] args) {
-        int num = 58;
+        int num = 3999;
         System.out.println(intToRoman(num));
     }
 }

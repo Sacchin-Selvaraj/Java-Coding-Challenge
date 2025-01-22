83ab995298b23629d36e0c8da847d53ef4141ac8
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-22 23:51:03 +0530

    Anagram

---
+++ src/main/java/Java_Program/SubDomainCount.java
@@ -0,0 +1,23 @@
+package Java_Program;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SubDomainCount {
+
+    public  static List<String> subdomainVisits(String[] cpdomains) {
+
+      
+        
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+
+        String[] cpdomains={"9001 discuss.leetcode.com"};
+        System.out.println(subdomainVisits(cpdomains));
+
+    }
+}

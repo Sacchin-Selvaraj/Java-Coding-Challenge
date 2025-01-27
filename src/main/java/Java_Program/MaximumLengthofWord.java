e07dcff86115a6decda12c916fd0f22d0f2613d2
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-27 10:24:19 +0530

    Strings

--- src/main/java/Java_Program/MaximumLengthofWord.java
+++ src/main/java/Java_Program/MaximumLengthofWord.java
@@ -9,34 +9,52 @@ public class MaximumLengthofWord {
 
     public static int maxProduct(String[] words) {
 
-        List<Set<Character>> list = new ArrayList<>();
-        for (String word : words) {
-            Set<Character> set = new HashSet<>();
-            for (int i = 0; i < word.length(); i++) {
-                set.add(word.charAt(i));
+//        List<Set<Character>> list = new ArrayList<>();
+//        for (String word : words) {
+//            Set<Character> set = new HashSet<>();
+//            for (int i = 0; i < word.length(); i++) {
+//                set.add(word.charAt(i));
+//            }
+//            list.add(set);
+//        }
+//        int max = 0;
+//        for (int i = 0; i < words.length; i++) {
+//            Set<Character> set1 = list.get(i);
+//            for (int j = i + 1; j < words.length - 1; j++) {
+//                boolean res = false;
+//                Set<Character> set2 = list.get(j);
+//                for (char c :set1) {
+//                    if (set2.contains(c)) {
+//                        res = true;
+//                        break;
+//                    }
+//                }
+//                if (!res) {
+//                    max = Math.max(max, words[i].length()*words[j].length());
+//                }
+//            }
+//        }
+//        return max;
+            if (words == null || words.length == 0)
+                return 0;
+            int len = words.length;
+            int[] value = new int[len];
+            for (int i = 0; i < len; i++) {
+                String tmp = words[i];
+                value[i] = 0;
+                for (int j = 0; j < tmp.length(); j++) {
+                    value[i] |= 1 << (tmp.charAt(j) - 'a');
+                }
             }
-            list.add(set);
+            int maxProduct = 0;
+            for (int i = 0; i < len; i++)
+                for (int j = i + 1; j < len; j++) {
+                    if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
+                        maxProduct = words[i].length() * words[j].length();
+                }
+            return maxProduct;
         }
-        int max = 0;
-        for (int i = 0; i < words.length; i++) {
-            Set<Character> set1 = list.get(i);
-            for (int j = i + 1; j < words.length - 1; j++) {
-                boolean res = false;
-                Set<Character> set2 = list.get(j);
-                for (char c :set1) {
-                    if (set2.contains(c)) {
-                        res = true;
-                        break;
-                    }
-                }
-                if (!res) {
-                    max = Math.max(max, words[i].length()*words[j].length());
-                }
 
-            }
-        }
-        return max;
-    }
 
     public static void main(String[] args) {
 

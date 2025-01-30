578c09d2db219f7582bb6b5ddbb5c070b9ab62ff
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-01-30 22:18:11 +0530

    Strings

--- src/main/java/Java_Program/CountingValleys.java
+++ src/main/java/Java_Program/CountingValleys.java
@@ -2,7 +2,20 @@ package Java_Program;
 
 public class CountingValleys {
 
-    String str="DUDDDUDUUU";
 
 
+    public static void main(String[] args) {
+        String str="UDDDUDUU";
+        int res=0,count=0;
+        for (int i = 0; i < str.length(); i++) {
+            if(i!=0&&(res==0&&str.charAt(i-1)=='U')){
+                count++;
+            }else if(str.charAt(i)=='D'){
+                res--;
+            }else {
+                res++;
+            }
+        }
+        System.out.println(count);
+    }
 }

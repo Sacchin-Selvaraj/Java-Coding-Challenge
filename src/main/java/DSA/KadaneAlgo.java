df796b8e7990f470e7f9d9225d6ddab6b8b5ebd6
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-01 20:11:26 +0530

    Arrays

--- src/main/java/DSA/KadaneAlgo.java
+++ src/main/java/DSA/KadaneAlgo.java
@@ -3,17 +3,23 @@ package DSA;
 public class KadaneAlgo {
 
     public static int maxSubArray(int[] nums) {
-        int sum=0,max=Integer.MIN_VALUE,count=0;
+        int sum=0,max=Integer.MIN_VALUE,count=0,start=0,end=0;
+        int actualStart=0;
         for (int i = 0; i < nums.length; i++) {
+            if (sum==0)
+                start=i;
             sum=sum+nums[i];
             if(sum<0) {
                 sum = 0;
                 count++;
             }
-            if(sum>max)
-                max=sum;
+            if(sum>max) {
+                max = sum;
+                actualStart=start;
+                end=i;
+            }
         }
-
+        System.out.println("Start:"+actualStart+"  End:"+end);
         return count==nums.length?-1:max;
     }
 

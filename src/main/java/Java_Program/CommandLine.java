e08fa5cefe87bf51b267f419a6a0db9608f7f535
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-02 20:41:26 +0530

    Shift

---
+++ src/main/java/Java_Program/CommandLine.java
@@ -0,0 +1,10 @@
+package Java_Program;
+
+import java.util.Arrays;
+
+public class CommandLine {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
---
+++ src/main/java/Java_Program/GFG.java
@@ -0,0 +1,14 @@
+package Java_Program;
+
+public class GFG {
+
+    public static void main(String[] args)
+    {
+        int number = 2;
+
+        // 2 bit left shift operation
+        int Ans = number << 1;
+
+        System.out.println(Ans);
+    }
+}

a0384d9baa84b25c4bbea36609b0c43bbf1c2352
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-02 23:07:55 +0530

    Basics

--- src/main/java/Java_Program/GFG.java
+++ src/main/java/Java_Program/GFG.java
@@ -1,14 +1,39 @@
 package Java_Program;
 
-public class GFG {
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Computers {
+   abstract void turnOn();
+}
+
+interface Dell {
+    void display();
+}
+
+
+public class GFG  {
 
     public static void main(String[] args)
     {
         int number = 2;
+        List<Integer> list=new ArrayList<>();
+
 
         // 2 bit left shift operation
         int Ans = number << 1;
 
         System.out.println(Ans);
+
+
+    }
+
+    public static class Hello extends Computers{
+
+
+        @Override
+        void turnOn() {
+
+        }
     }
 }

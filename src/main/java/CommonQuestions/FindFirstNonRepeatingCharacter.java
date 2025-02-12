9f95a024f4e16c159ee2e1b10360bcc2394435b5
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-12 09:16:05 +0530

    Non Repeating Character

--- src/main/java/CommonQuestions/FindFirstNonRepeatingCharacter.java
+++ src/main/java/CommonQuestions/FindFirstNonRepeatingCharacter.java
@@ -1,6 +1,25 @@
 package CommonQuestions;
 
+import java.util.*;
+
 public class FindFirstNonRepeatingCharacter {
 
+    public static void main(String[] args) {
+        String str = "SacchinS";
+
+        HashMap<Character,Integer> map=new LinkedHashMap<>();
+        for (char ch:str.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        System.out.println(map);
+
+        for (Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()==1){
+                System.out.println(entry.getKey());
+                break;
+            }
+        }
+
+    }
 
 }

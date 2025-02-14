a2d3e227e3701a52cbd9ec755756fd0ed42f9122
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-14 15:18:52 +0530

    Boolean

---
+++ src/main/java/Java_Program/BooleanPratice.java
@@ -0,0 +1,15 @@
+package Java_Program;
+
+public class BooleanPratice {
+
+    public static void main(String[] args) {
+
+        boolean a=true;
+        boolean b=false;
+
+        if(a==b){
+            System.out.println("Hi");}
+        else
+            System.out.println("Hello");
+        }
+    }

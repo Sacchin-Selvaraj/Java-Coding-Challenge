7b178f1d14f7b2c472e94d73fc317e043638aadd
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-15 11:34:05 +0530

    HashMap

--- src/main/java/Java_Program/OccuranceOfElement.java
+++ src/main/java/Java_Program/OccuranceOfElement.java
@@ -6,27 +6,32 @@ import java.util.HashMap;
 public class OccuranceOfElement {
     public static int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
 
-        int[] res=new int[queries.length];
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for (int j:nums){
-            map.put(j,map.getOrDefault(j,0)+1);
+        int[] res = new int[queries.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int counter = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == x) {
+                map.put(counter, i);
+                counter++;
+            }
         }
-        int freq=map.getOrDefault(x,-1);
+
         for (int i = 0; i < queries.length; i++) {
-            if (queries[i]<=freq){
-                res[i]=queries[i];
-            }else
+            if(!map.containsKey(queries[i]))
                 res[i]=-1;
+            else
+                res[i]=map.get(queries[i]);
         }
+
         return res;
     }
 
 
     public static void main(String[] args) {
-        int[] nums={1,3,1,7};
-        int[] queries={1,3,2,4};
-        int x=1;
+        int[] nums = {1, 3, 1, 7};
+        int[] queries = {1, 3, 2, 4};
+        int x = 1;
 
-        System.out.println(Arrays.toString(occurrencesOfElement(nums,queries,x)));
+        System.out.println(Arrays.toString(occurrencesOfElement(nums, queries, x)));
     }
 }

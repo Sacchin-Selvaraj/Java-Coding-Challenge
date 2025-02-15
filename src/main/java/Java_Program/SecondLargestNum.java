d2f4c6dbd01e20ced6fa849bbbe58688445c7b7f
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-02-15 12:07:25 +0530

    Array

--- src/main/java/Java_Program/SecondLargestNum.java
+++ src/main/java/Java_Program/SecondLargestNum.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class SecondLargestNum{
 
     public static void main(String[] args) {
-        int[] arr={5,9,2,4,8,1,6};
+        int[] arr={5,9,2,4,8,1,6,9,8,9,8,8,7,9,7,9};
 
         PriorityQueue<Integer> queue=new PriorityQueue<>((a,b)->b-a);
         for (int num:arr){
@@ -19,5 +19,18 @@ public class SecondLargestNum{
 
        Optional<Integer> num= Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).skip(1).findFirst();
         System.out.println("Result "+num);
+
+        int largest=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        for (int nums:arr){
+            if(nums>largest){
+                second=largest;
+                largest=nums;
+            } else if (nums>second && nums!=largest) {
+                second=nums;
+            }
+        }
+
+        System.out.println("Output "+second);
     }
 }

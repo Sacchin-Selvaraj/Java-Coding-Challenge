4442c4d6321db5614702d77def0b582a43d53a81
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-03-06 13:39:27 +0530

    Streams

---
+++ src/main/java/Java_Program/FindSecondNumber.java
@@ -0,0 +1,29 @@
+package Java_Program;
+
+public class FindSecondNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 2, 9, 7, 1, 1, 3, 4, 9, 9, 1};
+
+        int secondMin = Integer.MAX_VALUE, secondMax = Integer.MIN_VALUE, firstMax = Integer.MIN_VALUE, firstMin = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > firstMax) {
+                secondMax = firstMax;
+                firstMax = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != firstMax) {
+                secondMax = arr[i];
+            }
+
+            if (arr[i] < firstMin) {
+                secondMin = firstMin;
+                firstMin = arr[i];
+            } else if (arr[i] < secondMin && arr[i] != firstMin) {
+                secondMin = arr[i];
+            }
+
+        }
+        System.out.println(secondMax);
+        System.out.println("Second Min " + secondMin);
+    }
+}

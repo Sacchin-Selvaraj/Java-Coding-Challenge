5525bb6b5a41d05ba7c01c1f7c5fcf98a72c0ebe
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-03-16 12:41:47 +0530

    Array

--- src/main/java/Contest/CloseEqualElements.java
+++ src/main/java/Contest/CloseEqualElements.java
@@ -1,32 +1,53 @@
 package Contest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CloseEqualElements {
     public static List<Integer> solveQueries(int[] nums, int[] queries) {
-
-        List<Integer> list=new ArrayList<>();
-        for (int i = 0; i < queries.length; i++) {
-            int num=calculateElements(nums,queries[i]);
-            list.add(num);
+        int n = nums.length;
+        HashMap<Integer, ArrayList<Integer>> mh = buildMap(nums, n);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int q : queries) {
+            res.add(solve(mh.get(nums[q]), q, n));
         }
-        return list;
+        return res;
     }
 
-    private static int calculateElements(int[] nums, int query) {
-        int Min=Integer.MAX_VALUE;
-        int n=nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i]==nums[query]&&i!=query){
-                int forward = (i - query + n) % n; // Forward distance
-                int backward = (query - i + n) % n; // Backward distance
-                int distance = Math.min(forward, backward);
-                Min = Math.min(Min, distance);
+    public static HashMap<Integer, ArrayList<Integer>> buildMap(int[] nums, int n) {
+        HashMap<Integer, ArrayList<Integer>> mh = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!mh.containsKey(nums[i])) {
+                mh.put(nums[i], new ArrayList<>());
             }
+            mh.get(nums[i]).add(i);
         }
+        return mh;
+    }
 
-        return (Min==Integer.MAX_VALUE)?-1:Min;
+    public static int solve(ArrayList<Integer> l, int q, int n) {
+        if (l == null || l.size() < 2) return -1;
+        int pos = binarySearch(l, q);
+        int left = pos == 0 ? l.get(l.size() - 1) : l.get(pos - 1);
+        int right = pos == l.size() - 1 ? l.get(0) : l.get(pos + 1);
+        return Math.min(dist(q, left, n), dist(q, right, n));
+    }
+
+    public static int binarySearch(ArrayList<Integer> l, int target) {
+        int left = 0, right = l.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (l.get(mid) == target) return mid;
+            else if (l.get(mid) < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+
+    public static int dist(int i, int j, int n) {
+        int d = Math.abs(i - j);
+        return Math.min(d, n - d);
     }
 
     public static void main(String[] args) {

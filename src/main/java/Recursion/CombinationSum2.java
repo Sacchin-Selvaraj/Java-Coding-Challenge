d36ee372c79a6965ea39fd35fcd740803f134d63
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-04-15 22:23:12 +0530

    Streams 15 Apr

--- src/main/java/Recursion/CombinationSum2.java
+++ src/main/java/Recursion/CombinationSum2.java
@@ -1,4 +1,38 @@
 package Recursion;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CombinationSum2 {
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinationSum(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
+
+    private static void findCombinationSum(List<List<Integer>> list, List<Integer> temp, int[] candidates, int target, int idx) {
+        if (target == 0) {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = idx; i < candidates.length; i++) {
+            if (i>idx && candidates[i]==candidates[i-1])
+                continue;
+            if (candidates[i]>target)
+                break;
+            temp.add(candidates[i]);
+            findCombinationSum(list, temp, candidates, target - candidates[i], i + 1);
+            temp.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = {2, 5, 2, 1, 2}; // [1,2,2,2,5]
+        int target = 5;
+        System.out.println(combinationSum2(candidates, target));
+    }
 }
--- src/main/java/Streams/SumEvenNumbers.java
+++ src/main/java/Streams/SumEvenNumbers.java
@@ -2,13 +2,21 @@ package Streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SumEvenNumbers {
     public static void main(String[] args) {
-        List<Integer> numbers= Arrays.asList(1,2,3,4,5,6,7,8,10);
+        List<Integer> numbers= Arrays.asList(1,2,3,4);
 
        Optional<Integer> sum=numbers.stream().filter(num->num%2==1).reduce(Integer::sum);
         System.out.println(sum.get());
+
+        Map<Boolean,Integer> map=numbers.stream().collect(Collectors.partitioningBy(num->num%2==0
+                ,Collectors.summingInt(Integer::intValue)));
+
+        System.out.println(map);
+
     }
 }

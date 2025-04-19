b9b465d8912096b47f861fd7b4e0ee1c48982126
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-04-19 15:44:01 +0530

    Patterns 19 Apr

---
+++ src/main/java/StarPatterns/Pattern1.java
@@ -0,0 +1,22 @@
+package StarPatterns;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+
+        int n = 20;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= n -(2*i); j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}

ef52ce8d7056f223b4aa874dcbe16920cb8a478c
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-05-25 19:05:47 +0530

    Minimum Log Transportation 25 May

--- src/main/java/Contest/MinimumLogTransportation.java
+++ src/main/java/Contest/MinimumLogTransportation.java
@@ -4,14 +4,36 @@ package Contest;
 public class MinimumLogTransportation {
 
     public static long minCuttingCost(int n, int m, int k) {
-        int cost=0;
-        return 0;
+        long cost=1;
+        if (n>k){
+            int mod=n%k;
+            if (mod==0) {
+                mod = n - k;
+                n=n-k;
+            }
+            else
+                n=n-mod;
+            cost=cost*n*mod;
+        } else if (m>k) {
+            int mod=m%k;
+            if (mod==0) {
+                mod = m - k;
+                m=m-k;
+            }
+            else
+                 m=m-mod;
+            cost=cost*m*mod;
+        }else {
+            return 0;
+        }
+        return cost;
+
     }
 
     public static void main(String[] args) {
-        int n=6;
-        int m=5;
-        int k=5;
+        int n=49898;
+        int m=109372;
+        int k=62703;
         System.out.println(minCuttingCost(n,m,k));
     }
 }

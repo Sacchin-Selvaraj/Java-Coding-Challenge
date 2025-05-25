14824b76ce9436f47457157e25c9aff81550a0a2
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-05-25 13:41:30 +0530

    Minimum log Transportation

---
+++ src/main/java/Contest/MinimumLogTransportation.java
@@ -0,0 +1,17 @@
+
+package Contest;
+
+public class MinimumLogTransportation {
+
+    public static long minCuttingCost(int n, int m, int k) {
+        int cost=0;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int n=6;
+        int m=5;
+        int k=5;
+        System.out.println(minCuttingCost(n,m,k));
+    }
+}

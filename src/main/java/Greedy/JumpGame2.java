6c5f5b834cdbe9b2144c7985fa5f93eaf6fdc7a4
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-05-28 12:37:37 +0530

    Jum game 2 28 may

--- src/main/java/Greedy/JumpGame2.java
+++ src/main/java/Greedy/JumpGame2.java
@@ -2,9 +2,9 @@ package Greedy;
 
 public class JumpGame2 {
 
-    public static int jump(int[] nums) {
+    public static int jumps(int[] nums) {
 
-        return recursiveJump(nums,0);
+        return recursiveJump(nums, 0);
     }
 
     private static int recursiveJump(int[] nums, int idx) {
@@ -27,7 +27,24 @@ public class JumpGame2 {
     }
 
     public static void main(String[] args) {
-        int[] nums={2,3,1,1,4};
+        int[] nums = {2, 3, 1, 1, 4};
+        //System.out.println(jumps(nums));
         System.out.println(jump(nums));
     }
+
+    public static int jump(int[] nums) {
+
+        int left = 0, right = 0, jumps = 0;
+        while (right < nums.length - 1) {
+            int farthest = 0;
+            for (int i = left; i <= right; i++) {
+                farthest=Math.max(farthest,i+nums[i]);
+            }
+            left = right + 1;
+            right = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+
 }

c33d4dcb1d585c358ffd4b0f3b70e7884a400b00
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-07-04 13:54:14 +0530

    Detect Cycle in BFS 04 Jul

---
+++ src/main/java/Graph/DetectCycleinUnDirectedGraphBFS.java
@@ -0,0 +1,54 @@
+package Graph;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class DetectCycleinUnDirectedGraphBFS {
+
+    private static boolean detectCycleBFS(List<List<Integer>> list, int nodes) {
+
+        Queue<int[]> queue=new ArrayDeque<>();
+        boolean[] vis =new  boolean[nodes+1];
+        queue.add(new int[]{1,0});
+        while (!queue.isEmpty()){
+            int[] pair=queue.poll();
+            vis[pair[0]]=true;
+            for (Integer node:list.get(pair[0])){
+                if (node==pair[1]){
+                    continue;
+                }
+                if (!vis[node]){
+                    queue.add(new int[]{node,pair[0]});
+                }else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> list=new ArrayList<>();
+        int nodes=7;
+        for (int i = 0; i <= nodes; i++) {
+            list.add(new ArrayList<>());
+        }
+        list.get(1).add(2);
+        list.get(1).add(3);
+        list.get(2).add(1);
+        list.get(2).add(5);
+        list.get(3).add(1);
+        list.get(3).add(4);
+        list.get(3).add(6);
+        list.get(4).add(3);
+        list.get(5).add(2);
+        list.get(5).add(7);
+        list.get(6).add(3);
+        list.get(6).add(7);
+        list.get(7).add(5);
+        list.get(7).add(6);
+        System.out.println(detectCycleBFS(list,nodes));
+    }
+}

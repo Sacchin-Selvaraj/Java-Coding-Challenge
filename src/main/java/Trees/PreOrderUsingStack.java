8a70d30454891bc814514922d0924bebb4431340
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-07-22 11:25:30 +0530

    Pre Order 22 Jul

---
+++ src/main/java/Trees/PreOrderUsingStack.java
@@ -0,0 +1,65 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+class PreOrder{
+
+    Node root=null;
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.val = data;
+        }
+    }
+    public Node addElement(Node root,int data){
+        if (root==null){
+            root=new Node(data);
+        } else if (data<root.val) {
+            root.left=addElement(root.left,data);
+        } else if (data> root.val) {
+            root.right=addElement(root.right,data);
+        }
+
+        return root;
+    }
+    public List<Integer> PreOrderStack(Node node){
+        List<Integer> list=new ArrayList<>();
+        if (node==null) return list;
+        Stack<Node> stack=new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()){
+            Node pair=stack.pop();
+            list.add(pair.val);
+            if (pair.right!=null){
+                stack.push(pair.right);
+            }
+            if (pair.left!=null){
+                stack.push(pair.left);
+            }
+        }
+        return list;
+    }
+
+}
+public class PreOrderUsingStack {
+
+    public static void main(String[] args) {
+        PreOrder obj=new PreOrder();
+        System.out.println("Enter the Number of Elements : ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        PreOrder.Node root = obj.root;
+        for (int i = 0; i < n; i++) {
+            int num=sc.nextInt();
+            root=obj.addElement(root,num);
+        }
+        // obj.InOrderTraversal(root);
+        System.out.println(obj.PreOrderStack(root));
+    }
+}

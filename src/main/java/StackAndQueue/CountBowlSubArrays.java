09da517e8a67350284116ae8cc180d56753dfec4
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-09-07 14:24:00 +0530

    Count Bowl Sub Arrays 07 Sep

---
+++ src/main/java/StackAndQueue/CountBowlSubArrays.java
@@ -0,0 +1,29 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class CountBowlSubArrays {
+
+    public static void main(String[] args) {
+        int[] nums={5,1,2,3,4};
+        System.out.println(bowlSubarrays(nums));
+    }
+    public static long bowlSubarrays(int[] A) {
+        long res = 0;
+        Stack<Integer> s = new Stack<>();
+        for (int r = 0; r < A.length; ++r) {
+            int a = A[r];
+            while (!s.empty() && A[s.peek()] <= a) {
+                int l = s.pop();
+                if (r - l + 1 >= 3) {
+                    res += 1;
+                }
+            }
+            if (!s.empty() && r - s.peek() + 1 >= 3) {
+                res += 1;
+            }
+            s.push(r);
+        }
+        return res;
+    }
+}

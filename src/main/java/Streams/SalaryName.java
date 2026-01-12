b734137db3893be10d9792a73d9795b883c77f5d
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-01-12 12:40:06 +0530

    Sort by Salary and Name

--- src/main/java/Streams/SalaryName.java
+++ src/main/java/Streams/SalaryName.java
@@ -1,8 +1,21 @@
 package Streams;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class SalaryName {
 
     public static void main(String[] args) {
+        Employee employee=new Employee();
 
+        List<Employee> employeeList = employee.getEmployees();
+
+        List<Employee> employeeListSorted = employeeList.stream()
+                .sorted(Comparator.comparingDouble(Employee::getSalary)
+                        .reversed()
+                        .thenComparing(employee1 -> employee1.name.toLowerCase()))
+                .toList();
+
+        employeeListSorted.forEach(employee1 -> System.out.println("\n"+employee1));
     }
 }

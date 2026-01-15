f3044a3330ffb64447084eb45a872857b5cbe1b7
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-01-15 17:06:27 +0530

    Count Occurance

--- src/main/java/Java_Program/CountOccurances.java
+++ src/main/java/Java_Program/CountOccurances.java
@@ -1,6 +1,8 @@
 package Java_Program;
 
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -23,10 +25,31 @@ public class CountOccurances {
         return res;
     }
     public static void main(String[] args) {
-        String s = "Sacchin";
+        String s = "Saccadfazaswsahin";
         System.out.println(areOccurrencesEqual(s));
         System.out.println();
         System.out.println("Occurance : "+ occuranceCharacter(s,'c'));
+        System.out.println(countOccurance(s));
+    }
+
+    private static Character countOccurance(String s) {
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for (char c:s.toCharArray()){
+            hashMap.put(c,hashMap.getOrDefault(c,0)+1);
+        }
+
+        char ch = ' ';
+        int maximumOccurance=0;
+
+        for (Map.Entry<Character,Integer> entry : hashMap.entrySet()){
+            if (entry.getValue()>maximumOccurance){
+                ch=entry.getKey();
+                maximumOccurance=entry.getValue();
+            }
+        }
+
+      return ch;
+
     }
 
     private static long occuranceCharacter(String s, char a) {
@@ -34,8 +57,6 @@ public class CountOccurances {
         return s.chars().filter(ch-> ch==a).count();
 
     }
-
-
 
 
 }

0a4445c068f466898c2315be11b5a29bd8852e5a
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-01-30 20:16:06 +0530

    MultiThreads

--- src/main/java/Threads/BasicThread.java
+++ src/main/java/Threads/BasicThread.java
@@ -1,10 +1,33 @@
 package Threads;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
 public class BasicThread {
 
     public static void main(String[] args) throws InterruptedException {
-       Thread thread=new Thread(new MyTask());
+        Thread thread=new Thread(new MyTask());
         thread.start();
+        MyTask task=new MyTask();
+        task.make()
+
+                .thenApply(String::toUpperCase)
+                .thenAccept(System.out::println);
+
+        List<Integer> numbers=List.of(1,3,6,8,9,12);
+
+        System.out.println(numbers);
+
+        List<Integer> numberList=new ArrayList<>();
+
+        Map<Integer,Integer> numberMap=new HashMap<>();
+
+        int[] arr=numbers.stream().mapToInt(num->num).toArray();
+        System.out.println(numbers.size());
     }
 
 }
@@ -14,9 +37,16 @@ class MyTask implements Runnable{
     @Override
     public void run() {
         long time=System.currentTimeMillis();
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Number : "+i);
         }
         System.out.println(System.currentTimeMillis()-time);
+    }
+
+    public CompletableFuture<String> make(){
+        System.out.println("Sacchin");
+        return CompletableFuture.supplyAsync(() -> {
+            return "Sacchin";
+        });
     }
 }

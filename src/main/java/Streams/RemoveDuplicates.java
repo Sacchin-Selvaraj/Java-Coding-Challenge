4a8c55ab69b5599caf7ef9a24d0eb858a53cd4f6
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-02-07 11:30:27 +0530

    Remove Duplicates

--- src/main/java/Streams/RemoveDuplicates.java
+++ src/main/java/Streams/RemoveDuplicates.java
@@ -7,26 +7,29 @@ import java.util.stream.Collectors;
 public class RemoveDuplicates {
 
     public static void main(String[] args) {
-        String str = "Hello Java";
-        String str1 = str.chars().mapToObj(value -> (char) value)
-                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new,Collectors.counting()))
-                .keySet().stream().map(String::valueOf).collect(Collectors.joining());
+//        String str = "Hello Java";
+//        String str1 = str.chars().mapToObj(value -> (char) value)
+//                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new,Collectors.counting()))
+//                .keySet().stream().map(String::valueOf).collect(Collectors.joining());
+//
+//        System.out.println(str1);
+//
+//        String str2 = str.chars().distinct().mapToObj(value -> (char) value).collect(StringBuilder::new,StringBuilder::append,StringBuilder::append).toString();
+//        System.out.println(str2);
+//
+//        String str3 = str.chars().distinct().mapToObj(value -> String.valueOf((char) value)).collect(Collectors.joining());
+//        System.out.println(str3);
+//
+//        int[] num1={3,1,2};
+//        int[] num2={5,4,6};
+//
+//        List<Integer> list=new ArrayList<>();
 
-        System.out.println(str1);
+        String str6="Sacchin";
 
-        String str2 = str.chars().distinct().mapToObj(value -> (char) value).collect(StringBuilder::new,StringBuilder::append,StringBuilder::append).toString();
-        System.out.println(str2);
+        Map<Character,Long> map = str6.chars().mapToObj(ch->(char)ch)
+                .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()));
 
-        String str3 = str.chars().distinct().mapToObj(value -> String.valueOf((char) value)).collect(Collectors.joining());
-        System.out.println(str3);
-
-        int[] num1={3,1,2};
-        int[] num2={5,4,6};
-
-        List<Integer> list=new ArrayList<>();
-
-        int[] sortedArr= Arrays.stream(num1).sorted().toArray();
-
-
+        System.out.println(map);
     }
 }

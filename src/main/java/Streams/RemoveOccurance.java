8abf4d6733cffd03a2766b8c40e94758f427d186
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-02-26 13:53:50 +0530

    Remove Occurance 26 Feb

--- src/main/java/Streams/RemoveOccurance.java
+++ src/main/java/Streams/RemoveOccurance.java
@@ -7,8 +7,18 @@ public class RemoveOccurance {
     public static void main(String[] args) {
         String str = "Java Interview";
 
-        String occuranceRemoved = str.chars().filter(word->word!='e').mapToObj(c->String.valueOf((char) c)).collect(Collectors.joining());
+        String occuranceRemoved = str.chars()
+                .filter(word->word!='e')
+                .mapToObj(c->String.valueOf((char) c))
+                .collect(Collectors.joining());
+
         System.out.println(occuranceRemoved);
+
+        String occuranceRemoveds = str.chars().mapToObj(ch-> (char)ch)
+                .filter(ch-> ch!='a')
+                .collect(StringBuilder::new,StringBuilder::append,StringBuilder::append).toString();
+
+        System.out.println(occuranceRemoveds);
 
     }
 }

149e5c5ca93c0fda91b0437cda8f50da888ce192
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-02-28 20:28:36 +0530

    Smallest Pair 28 Feb

---
+++ src/main/java/Contest/SmallestPair.java
@@ -0,0 +1,50 @@
+package Contest;
+
+import java.util.*;
+
+public class SmallestPair {
+
+    public  static int[] minDistinctFreqPair(int[] nums) {
+
+        Map<Integer,Integer> freqMap=new HashMap<>();
+        TreeSet<Integer> set=new TreeSet<>();
+
+        for(int num:nums){
+            freqMap.put(num,freqMap.getOrDefault(num,0)+1);
+            set.add(num);
+        }
+
+        int a=-1,b=-1;
+        int freqA=0,freqB=0;
+
+        for(int num:set){
+            if(a==-1 && freqA==0){
+                int x=freqMap.get(num);
+                a=num;
+                freqA=x;
+                continue;
+            }
+
+            if(freqMap.get(num)==freqA){
+                continue;
+            }else{
+                b=num;
+                freqB=freqMap.get(num);
+                break;
+            }
+
+        }
+
+        if(b==-1){
+            return new int[] {-1,-1};
+        }
+
+        return new int[] {a,b};
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,5};
+        System.out.println(Arrays.toString(minDistinctFreqPair(nums)));
+    }
+}

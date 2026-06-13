1890319590de65d7301d5824fbeee9a822d26bb7
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2026-06-13 14:03:24 +0530

    Freq String By Value

---
+++ src/main/java/Streams/FrequencyStringByValue.java
@@ -0,0 +1,28 @@
+package Streams;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FrequencyStringByValue {
+    public static void main(String[] args) {
+
+        String str = "java is a good coding language";
+
+        Map<Character,Long> freqMap = str.chars()
+                .mapToObj(c->(char)c)
+                .filter(c-> c !=' ')
+                .collect(Collectors.groupingBy(c->c,
+                        Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<Character,Long>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.<Character,Long>comparingByKey().reversed()))
+                .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,
+                        (e, r)->e, LinkedHashMap::new
+                ));
+
+        System.out.println(freqMap);
+    }
+}
--- src/main/java/Threads/OddEvenNumber.java
+++ src/main/java/Threads/OddEvenNumber.java
@@ -49,6 +49,7 @@ public class OddEvenNumber {
             }
 
             OddEvenNumber.class.notifyAll();
+
         }
     }
 }
